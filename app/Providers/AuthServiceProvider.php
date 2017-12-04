b4035034d8b4c78bd02822e1d097606a5593d1a2
Billy Rennekamp <billy.rennekamp@gmail.com>
2017-12-04 15:16:54 +0100

    fallback function

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -40,6 +40,20 @@ class AuthServiceProvider extends ServiceProvider
 
 
     public function checkHeader() {
+      if (!function_exists('apache_request_headers')) { 
+        function apache_request_headers() { 
+          foreach($_SERVER as $key=>$value) { 
+            if (substr($key,0,5)=="HTTP_") { 
+                $key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5))))); 
+                $out[$key]=$value; 
+            }else{ 
+                $out[$key]=$value; 
+            } 
+          } 
+          return $out; 
+        } 
+      } 
+
 
       $requestHeaders = apache_request_headers();
 
